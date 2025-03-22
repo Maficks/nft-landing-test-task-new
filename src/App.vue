62ae5cff21a34e92876ba9dcedf8a64b11a7c13d
Maficks <berrrserk2020@mail.ru>
2025-03-23 03:06:49 +0500

    initializing

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<script setup>
+
+</script>
+
+<template>
+  <header class="grid grid-rows-2">
+    <div class="main-header-bar bg-purple-300">
+      <img alt="logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+      <div class="main-header-bar-buttons">
+        <button>DISCOVER</button>
+        <button>CREATORS</button>
+        <button>SELL</button>
+        <button>STATS</button>
+      </div>
+    </div>
+    <div class="button-header-bar flex justify-end w-auto h-auto bg-amber-600">
+      <button>CONNECT WALLET</button>
+    </div>
+  </header>
+
+
+  <main>
+
+  </main>
+
+  <footer>
+
+  </footer>
+</template>
+
+<style scoped>
+/* header {
+
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+*/
+</style>
